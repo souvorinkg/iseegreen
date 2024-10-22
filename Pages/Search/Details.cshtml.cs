390bd2cd0240fcec82518f60b64237e94d7efe89
Kolya Souvorin <souvorinkg@hendrix.edu>
2024-10-22 13:50:16 -0500

    Got some API calls up, but I'm still trying to get the right tree for the given ID.

--- Pages/Search/Details.cshtml.cs
+++ Pages/Search/Details.cshtml.cs
@@ -8,6 +8,11 @@ using Microsoft.EntityFrameworkCore;
 using csci340_iseegreen.Data;
 using csci340_iseegreen.Models;
 using Microsoft.AspNetCore.Components.Web;
+using System.Data;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.ComponentModel;
+
 
 namespace csci340_iseegreen.Pages_Search
 {
@@ -28,8 +33,12 @@ namespace csci340_iseegreen.Pages_Search
 
         public string Error {get; set;} = default!;
 
+        public int id {get; set;} = 1;
+
         public async Task<IActionResult> OnGetAsync(string KewID)
         {
+            await MakeApiCall();
+
             IQueryable<csci340_iseegreen.Models.Taxa> taxaIQ = from t in _context.Taxa.Include(g => g.Genus).Include(f => f.Genus!.Family).Include(c => c.Genus!.Family!.Category) select t;
 
             taxaIQ = taxaIQ.Where(s => s.KewID.Equals(KewID));
@@ -54,6 +63,7 @@ namespace csci340_iseegreen.Pages_Search
         }
 
         public async Task<IActionResult> OnPostAddList(string KewID, int? list) {
+            await MakeApiCall();
             if (list == null) {
                 Error = "You have to select a list.";
                 IQueryable<csci340_iseegreen.Models.Taxa> taxaIQ = from t in _context.Taxa.Include(g => g.Genus).Include(f => f.Genus!.Family).Include(c => c.Genus!.Family!.Category) select t;
@@ -115,11 +125,104 @@ namespace csci340_iseegreen.Pages_Search
                 ListID = list.Value,
                 TimeDiscovered = DateTime.Now
             };
+            await MakeApiCall();
 
             await _context.AddAsync(item);
             await _context.SaveChangesAsync();
 
             return RedirectToPage("/ListItems/Index", new { itemid = list.ToString() });
         }
+
+
+public async Task MakeApiCall()
+{
+    using (var client = new HttpClient())
+    {
+        try
+        {
+            string url = $"https://perenual.com/api/species-list?key=sk-il1O6717dcf920ca97383&q=Monstera deliciosa";
+
+            // Make the GET request
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            var responseData = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("Successfully got data");
+
+            // Parse the JSON response
+            var jsonObject = JObject.Parse(responseData);
+
+            // Extract the "data" array
+            var data = jsonObject["data"] as JArray;
+
+            if (data != null && data.Count > 0)
+            {
+                // Get the first entry in the "data" array
+                var firstEntry = data[0];
+
+                // Extract the "id" field from the first entry
+                var id = firstEntry["id"]?.ToString();
+                
+                // Print the extracted ID
+                Console.WriteLine("First entry ID: " + id);
+                if (id != null)
+                {
+                    url = $"https://perenual.com/api/species/details/5257?key=sk-il1O6717dcf920ca97383";
+                    //url = $"https://perenual.com/api/species/details/{id}?key=sk-il1O6717dcf920ca97383";
+                    response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+                    responseData = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("Successfully got specific data");
+                    
+                    string description = GetDescription(responseData);
+                    ViewData["description"] = description;
+                    Console.WriteLine("Description: " + description);
+                    Console.WriteLine("Successfully got description");
+
+                }
+                else
+                {
+                    Console.WriteLine("No ID found for the given query.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No data found for the given query.");
+            }
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine($"Request error: {e.Message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
+}
+
+
+
+        public string GetDescription(string responseData)
+        {
+            // Parse the JSON response data
+            var jsonObject = JObject.Parse(responseData);
+
+            // Extract the "description" field
+            var description = jsonObject["description"]?.ToString();
+
+            return description ?? "Description not available";
+        }
+
+        // private int getIDfromName(string name, response)
+        // {
+        //     string url = $"https://perenual.com/api/species-list?key=sk-il1O6717dcf920ca97383&q=Monstera deliciosa";
+        //     var response = client.GetAsync(url);
+        //     response.EnsureSuccessStatusCode();
+        //     var responseData = await response.Content.ReadAsStringAsync();
+        //     var jsonObject = JObject.Parse(responseData);
+        //     int id = 1;
+        //     id = (int)jsonObject[0]["id"];
+        //     return id;
+        // }
     }
 }
